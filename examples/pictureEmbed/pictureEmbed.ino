18c91e0c8209fe3ec53306f99d08ec18d5591d84
Phillip Burgess <paintyourdragon@dslextreme.com>
2017-03-26 15:58:04 -0700

    Restored image conversion explanation in pictureEmbed.ino

--- examples/pictureEmbed/pictureEmbed.ino
+++ examples/pictureEmbed/pictureEmbed.ino
@@ -2,6 +2,22 @@
 // WILL NOT FIT ON ARDUINO UNO OR OTHER AVR BOARDS;
 // uses large bitmap image stored in array!
 
+// Options for converting images to the format used here include:
+//   http://www.rinkydinkelectronics.com/t_imageconverter565.php
+// or
+//  GIMP (https://www.gimp.org/) as follows:
+//    1. File -> Export As
+//    2. In Export Image dialog, use 'C source code (*.c)' as filetype.
+//    3. Press export to get the export options dialog.
+//    4. Type the desired variable name into the 'prefixed name' box.
+//    5. Uncheck 'GLIB types (guint8*)'
+//    6. Check 'Save as RGB565 (16-bit)'
+//    7. Press export to save your image.
+//  Assuming 'image_name' was typed in the 'prefixed name' box of step 4,
+//  you can have to include the c file, then using the image can be done with:
+//    tft.drawBitmap(0, 0, image_name.width, image_name.height, (uint16_t*)(image_name.pixel_data));
+//  See also https://forum.pjrc.com/threads/35575-Export-for-ILI9341_t3-with-GIMP
+
 #include "SPI.h"
 #include <Adafruit_ILI9341.h>
 #include "dragon.h"
