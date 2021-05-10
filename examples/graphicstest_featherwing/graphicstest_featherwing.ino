0c3646f184522533d52aad622507df0074e4433f
lady ada <limor@ladyada.net>
2021-05-10 10:36:58 -0400

    fixed up ifdefs to be clearer

--- examples/graphicstest_featherwing/graphicstest_featherwing.ino
+++ examples/graphicstest_featherwing/graphicstest_featherwing.ino
@@ -21,41 +21,33 @@
    #define TFT_CS   0
    #define TFT_DC   15
    #define SD_CS    2
-#endif
-#ifdef ESP32
+#else if defined(ESP32)
    #define STMPE_CS 32
    #define TFT_CS   15
    #define TFT_DC   33
    #define SD_CS    14
-#endif
-#ifdef TEENSYDUINO
+#else if defined(TEENSYDUINO)
    #define TFT_DC   10
    #define TFT_CS   4
    #define STMPE_CS 3
    #define SD_CS    8
-#endif
-#ifdef ARDUINO_STM32_FEATHER
+#else if defined(ARDUINO_STM32_FEATHER)
    #define TFT_DC   PB4
    #define TFT_CS   PA15
    #define STMPE_CS PC7
    #define SD_CS    PC5
-#endif
-#ifdef ARDUINO_NRF52832_FEATHER /* BSP 0.6.5 and higher! */
+#else if defined(ARDUINO_NRF52832_FEATHER)  /* BSP 0.6.5 and higher! */
    #define TFT_DC   11
    #define TFT_CS   31
    #define STMPE_CS 30
    #define SD_CS    27
-#endif
-#if defined(ARDUINO_MAX32620FTHR) || defined(ARDUINO_MAX32630FTHR)
+#else if defined(ARDUINO_MAX32620FTHR) || defined(ARDUINO_MAX32630FTHR)
    #define TFT_DC   P5_4
    #define TFT_CS   P5_3
    #define STMPE_CS P3_3
    #define SD_CS    P3_2
-#endif
-
-// Anything else!
-#if defined (__AVR_ATmega32U4__) || defined(ARDUINO_SAMD_FEATHER_M0) || defined (__AVR_ATmega328P__) || \
-    defined(ARDUINO_SAMD_ZERO) || defined(__SAMD51__) || defined(__SAM3X8E__) || defined(ARDUINO_NRF52840_FEATHER)
+#else
+    // Anything else, defaults!
    #define STMPE_CS 6
    #define TFT_CS   9
    #define TFT_DC   10
